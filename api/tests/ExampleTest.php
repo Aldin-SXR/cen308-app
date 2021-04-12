65600815575810d9a07948eccbdf7b590825837f
Aldin Kovačević <aldin.kovacevic.97@gmail.com>
2021-04-13 00:05:48 +0200

    Added PHPUnit and Travis.

---
+++ api/tests/ExampleTest.php
@@ -0,0 +1,28 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    protected $client;
+
+    protected function setUp(): void
+    {
+        $this->client = new GuzzleHttp\Client([
+            'base_uri' => array_key_exists('HEROKU', $_ENV) ? 
+                'https://cen308-test.herokuapp.com/api/' : 'http://localhost/cen308-app/api/'
+        ]);
+    }
+
+    public function testStatusRoute()
+    {
+        $response = $this->client->get('status');
+
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $data = json_decode($response->getBody(), true);
+
+        $this->assertArrayHasKey('status', $data);
+        $this->assertEquals('online', $data['status']);
+    }
+}
