9ecf1ca2661f43b8caf7b1b8340c5c884a08f1ae
Aldin Kovačević <aldin.kovacevic.97@gmail.com>
2021-04-13 15:21:22 +0200

    Added a simple test and Travis CI integration.

---
+++ api/tests/ExampleTest.php
@@ -0,0 +1,26 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    protected $client;
+
+    protected function setUp(): void
+    {
+        $this->client = new GuzzleHttp\Client([
+            'base_uri' => getenv('HEROKU') ? 'https://cen308-app.herokuapp.com/api/' : 'http://localhost/cen308-app/api/'
+        ]);
+    }
+
+    public function testStatusRoute()
+    {
+        $response = $this->client->get('status');
+
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $data = json_decode($response->getBody(), true);
+        $this->assertArrayHasKey('status', $data);
+        $this->assertEquals('online', $data['status']);
+    }
+}
